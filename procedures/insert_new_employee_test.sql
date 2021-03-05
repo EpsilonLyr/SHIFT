3e12e798cfce8b2fe195fe86256963cba38de181
EpsilonLyr <79781751+EpsilonLyr@users.noreply.github.com>
2021-03-05 15:37:22 +0700

    Update insert_new_employee_test.sql

--- procedures/insert_new_employee_test.sql
+++ procedures/insert_new_employee_test.sql
@@ -1,9 +1,5 @@
 do $$
 declare
-    p_fio varchar(50);
-	p_position varchar(30);
-    p_manager_id integer;
-	p_shop_id integer;
 begin
 call shop.insert_new_employee('fio', 'pos', null, 1);
 end $$;

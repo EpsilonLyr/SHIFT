ccfc855a254e3be259b43dc902fb227b7daa528b
EpsilonLyr <79781751+EpsilonLyr@users.noreply.github.com>
2021-03-12 01:37:54 +0700

    Update insert_product_to_shop_warehouse.sql

--- procedures/insert_product_to_shop_warehouse.sql
+++ procedures/insert_product_to_shop_warehouse.sql
@@ -5,9 +5,23 @@ create or replace procedure shop.insert_product_to_shop_warehouse(
 )
 as
 $$
+declare
+    p_count integer;
 begin
-	INSERT INTO shop.shop_warehouses(
-	id, shop_id, product_id, quantity)
-	VALUES (DEFAULT, p_shop_id, p_product_id, p_quantitie);
+ 	select count(*)
+    into p_count
+    from shop.shop_warehouses
+    where product_id=p_product_id and shop_id=p_shop_id;
+	if p_count > 0 then
+        update shop.shop_warehouses
+        set quantity = (quantity + p_quantity)
+        where product_id = p_product_id and shop_id = p_shop_id;
+    else
+        INSERT INTO shop.shop_warehouses(
+		id, shop_id, product_id, quantity)
+		VALUES (DEFAULT, p_shop_id, p_product_id, p_quantitie);
+    end if;
+	
+	
 end;
 $$ language plpgsql;
